d09a9366dd3f32b5f0c30e0d03695beed1954b23
玉村柊希 <syuuki0105@gmail.com>
2018-11-03 13:10:13 +0900

    体脂肪率を追加

---
+++ Assets/Scripts/Human/Body.cs
@@ -0,0 +1,38 @@
+﻿//
+// Body.cs
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ボディクラス
+/// </summary>
+public class Body : MonoBehaviour 
+{
+
+    // 体脂肪　
+    [SerializeField][Header("体脂肪")][Range(1, 100)]
+    private int m_fat = 20;
+
+
+    /// <summary>
+    /// 開始処理
+    /// </summary>
+    private void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    private void Update()
+    {
+        float scaleX = 5 - (100 / m_fat);
+        float scaleY = this.transform.localScale.y;
+        float scaleZ = 5 - (100 / m_fat);
+        this.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+    }
+}
---
+++ Assets/Scripts/Human/Parameter.cs
@@ -0,0 +1,61 @@
+﻿//
+// Parameter.cs
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ユーザーのパラメーター保持クラス
+/// </summary>
+public class Parameter
+{
+
+    // インスタンス
+    private static Parameter m_instance;
+
+    // 結果数値
+    private float m_resultValue;
+
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    private Parameter() { }
+
+    /// <summary>
+    /// クラスのインスタンスを取得
+    /// </summary>
+    /// <value>The instance.</value>
+    public static Parameter Instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new Parameter();
+            return m_instance;
+        }
+    }
+
+    /// <summary>
+    // 結果を作成する
+    /// </summary>
+    /// <returns>The parameter.</returns>
+    private float CreateResult()
+    {
+        m_resultValue = 0f;
+        return 0f;
+    }
+
+    #region プロパティ
+    /// <summary>
+    /// 結果の数値
+    /// </summary>
+    /// <value>The result value.</value>
+    public float ResultValue
+    {
+        get { return m_resultValue; }
+    }
+    #endregion
+}
